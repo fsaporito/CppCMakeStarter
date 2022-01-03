2ef2f79b41f49324396c8fe38d173803bb683493
fsaporito <sap93@gmail.com>
2022-01-03 19:19:28 +0100

    Add tools installation scripts

---
+++ scripts/cpp_tools_install.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+# Build Esstentials
+sudo apt install build-essential make
+
+# CMake
+sudo apt-get install cmake
+
+# CMake GUI
+sudo apt-get install cmake-gui
+
+# Doxygen
+sudo apt-get install doxygen
+sudo apt-get install graphviz
+
+# CCache
+sudo apt-get install ccache
+
+# CppCheck
+sudo apt-get install cppcheck
