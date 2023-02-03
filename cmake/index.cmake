1f6734a6132eba2468fe406106638d2bedc70f19
fsaporito <sap93@gmail.com>
2023-02-03 15:54:46 +0100

    Add scripts to cmake/index.cmake

--- cmake/index.cmake
+++ cmake/index.cmake
@@ -3,3 +3,5 @@ include_guard()
 set(CMAKE_MODULES_FOLDER ${CMAKE_MODULE_PATH})
 
 include(${CMAKE_MODULES_FOLDER}/create_optimization_flags.cmake)
+include(${CMAKE_MODULES_FOLDER}/create_warnings_list.cmake)
+include(${CMAKE_MODULES_FOLDER}/enable_static_analysis.cmake)
