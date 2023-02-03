8441a4766c72be8bb987e6b01701e364f1faae13
fsaporito <sap93@gmail.com>
2023-02-03 16:05:38 +0100

    Add missing tools in installation script

--- scripts/optional_cpp_tools_install.sh
+++ scripts/optional_cpp_tools_install.sh
@@ -7,6 +7,12 @@ sudo apt-get install graphviz
 # CCache
 sudo apt-get install ccache
 
+# Cmake Format
+sudo apt-get install cmake-format
+
 # CppCheck
 sudo apt-get install cppcheck
 sudo apt-get install cppcheck-gui
+
+# IWYU
+sudo apt-get install iwyu
