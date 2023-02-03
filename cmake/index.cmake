82633d0b22ba3349497deec1efdd3d3d1bb9174a
fsaporito <sap93@gmail.com>
2023-02-03 17:18:07 +0100

    Add conan

--- cmake/index.cmake
+++ cmake/index.cmake
@@ -1,6 +1,6 @@
 include_guard()
 
-set(CMAKE_MODULES_FOLDER ${CMAKE_MODULE_PATH})
+set(CMAKE_MODULES_FOLDER ${CMAKE_CUSTOM_MODULES_PATH})
 
 include(${CMAKE_MODULES_FOLDER}/create_optimization_flags.cmake)
 include(${CMAKE_MODULES_FOLDER}/create_warnings_list.cmake)
