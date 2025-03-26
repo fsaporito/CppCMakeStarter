2ee240b9020bbec0ce8e7cb73c57e0a607ba94c1
fsaporito <sap93@gmail.com>
2025-03-26 09:49:40 +0100

    Remove single line ifs

--- src/factorial/factorial.cpp
+++ src/factorial/factorial.cpp
@@ -4,16 +4,31 @@ int factorial::computeFact(const int factorialNumber)
 {
 
     if (factorialNumber == 0)
+    {
         return 1;
-    if (factorialNumber == 1)
+    }
+    else if (factorialNumber == 1)
+    {
         return 1;
-    if (factorialNumber == 2)
+    }
+    else if (factorialNumber == 2)
+    {
         return 2;
-    if (factorialNumber == 3)
+    }
+    else if (factorialNumber == 3)
+    {
         return 6;
+    }
     if (factorialNumber == 4)
+    {
         return 24;
+    }
     if (factorialNumber == 5)
+    {
         return 120;
-    return factorialNumber * computeFact(factorialNumber - 1);
+    }
+    else
+    {
+        return factorialNumber * computeFact(factorialNumber - 1);
+    }
 }
